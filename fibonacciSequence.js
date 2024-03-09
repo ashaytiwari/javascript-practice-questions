01754a9c921acbc8b3253ca96e04ec5e400d7642
ashaytiwari <ashaytiwari2001@gmail.com>
2024-03-09 22:07:42 +0530

    feat: added more functions for fibonacci sequence

--- fibonacciSequence.js
+++ fibonacciSequence.js
@@ -45,7 +45,7 @@ function fibonacciSeqUptoNTermsUsingRecursion(n) {
   let n1 = 0, n2 = 1;
   let index = 0;
 
-  (function childRecursive() {
+  (function childRecursiveFn() {
     console.log(n1);
 
     let nextTerm = n1 + n2;
@@ -57,7 +57,7 @@ function fibonacciSeqUptoNTermsUsingRecursion(n) {
       return;
     }
 
-    childRecursive(n1, n2);
+    childRecursiveFn(n1, n2);
 
   })();
 }
@@ -69,7 +69,7 @@ function fibonacciSeqUptoCertainNumberUsingRecursion(n) {
 
   let n1 = 0, n2 = 1;
 
-  (function childRecursive() {
+  (function childRecursiveFn() {
     console.log(n1);
 
     let nextTerm = n1 + n2;
@@ -80,10 +80,41 @@ function fibonacciSeqUptoCertainNumberUsingRecursion(n) {
       return;
     }
 
-    childRecursive(n1, n2);
+    childRecursiveFn(n1, n2);
 
   })();
 
 }
 
 fibonacciSeqUptoCertainNumberUsingRecursion(144);
+
+// Get fibonacci value at specific index
+function getFibonacciValueAtIndex(n) {
+
+  if (n < 2)
+    return 1;
+
+  return getFibonacciValueAtIndex(n - 1) + getFibonacciValueAtIndex(n - 2);
+
+}
+
+console.log(getFibonacciValueAtIndex(10));
+
+// memoized version of fibonacci sequence
+function getFibonacciValueAtIndexMemoized(n, memoCache) {
+
+  memoCache = memoCache || {};
+
+  if (memoCache[n]) {
+    return memoCache[n];
+  }
+
+  if (n < 2) {
+    return 1;
+  }
+
+  return getFibonacciValueAtIndexMemoized(n - 1, memoCache) + getFibonacciValueAtIndexMemoized(n - 2, memoCache);
+
+}
+
+console.log(getFibonacciValueAtIndexMemoized(10));
